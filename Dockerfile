c2c726e42a727ecbd27619e7a1818535be376517
minafsh <m.foosherian@yahoo.com>
2020-08-07 00:25:13 +0200

    two steps fallback policy

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ COPY actions /app/actions
 USER root
 RUN pip install --no-cache-dir -r /app/actions/requirements-actions.txt
 
+# Download spacy language data
+RUN python -m spacy download en_core_web_md
+RUN python -m spacy link en_core_web_md en
+
 USER 1001
 CMD ["start", "--actions", "actions", "--debug"]
 
--- actions/actions.py
+++ actions/actions.py
@@ -1,5 +1,6 @@
 from typing import Dict, Text, Any, List, Union, Optional
 import logging
+import json
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction, REQUESTED_SLOT
@@ -9,8 +10,11 @@ from rasa_sdk.events import (
     ActionExecuted,
     SessionStarted,
     Restarted,
+    UserUtteranceReverted,
+    ConversationPaused,
     FollowupAction,
 )
+
 from actions.parsing import (
     parse_duckling_time_as_interval,
     parse_duckling_time,
@@ -19,6 +23,8 @@ from actions.parsing import (
 )
 from actions.profile import create_mock_profile
 from dateutil import parser
+
+INTENT_DESCRIPTION_MAPPING_PATH = "actions/intent_description_mapping.csv"
 
 logger = logging.getLogger(__name__)
 
@@ -589,3 +595,147 @@ class ActionRestart(Action):
     ) -> List[EventType]:
 
         return [Restarted(), FollowupAction("action_session_start")]
+
+
+class ActionPause(Action):
+    """Pause the conversation"""
+
+    def name(self) -> Text:
+        return "action_pause"
+
+    def run(self, dispatcher, tracker, domain) -> List[EventType]:
+        return [ConversationPaused()]
+
+
+class ActionDefaultAskAffirmation(Action):
+    """Asks for an affirmation of the intent if NLU threshold is not met."""
+
+    def name(self) -> Text:
+        return "action_default_ask_affirmation"
+
+    def __init__(self) -> None:
+        import pandas as pd
+
+        self.intent_mappings = pd.read_csv(INTENT_DESCRIPTION_MAPPING_PATH)
+        self.intent_mappings.fillna("", inplace=True)
+        self.intent_mappings.entities = self.intent_mappings.entities.map(
+            lambda entities: {e.strip() for e in entities.split(",")}
+        )
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+
+        intent_ranking = tracker.latest_message.get("intent_ranking", [])
+        if len(intent_ranking) > 1:
+            diff_intent_confidence = intent_ranking[0].get(
+                "confidence"
+            ) - intent_ranking[1].get("confidence")
+            if diff_intent_confidence < 0.2:
+                intent_ranking = intent_ranking[:2]
+            else:
+                intent_ranking = intent_ranking[:1]
+
+        # for the intent name used to retrieve the button title, we either use
+        # the name of the name of the "main" intent, or if it's an intent that triggers
+        # the response selector, we use the full retrieval intent name so that we
+        # can distinguish between the different sub intents
+        first_intent_names = [
+            intent.get("name", "")
+            if intent.get("name", "")
+            not in ["out_of_scope", "faq", "chitchat"]
+            else tracker.latest_message.get("response_selector")
+            .get(intent.get("name", ""))
+            .get("full_retrieval_intent")
+            for intent in intent_ranking
+        ]
+
+        message_title = (
+            "Sorry, I'm not sure I've understood "
+            "you correctly 🤔 Do you mean..."
+        )
+
+        entities = tracker.latest_message.get("entities", [])
+        entities = {e["entity"]: e["value"] for e in entities}
+
+        entities_json = json.dumps(entities)
+
+        buttons = []
+        for intent in first_intent_names:
+            button_title = self.get_button_title(intent, entities)
+            if "/" in intent:
+                # here we use the button title as the payload as well, because you
+                # can't force a response selector sub intent, so we need NLU to parse
+                # that correctly
+                buttons.append(
+                    {"title": button_title, "payload": button_title}
+                )
+            else:
+                buttons.append(
+                    {
+                        "title": button_title,
+                        "payload": f"/{intent}{entities_json}",
+                    }
+                )
+
+        buttons.append(
+            {"title": "Something else", "payload": "/trigger_rephrase"}
+        )
+
+        dispatcher.utter_message(text=message_title, buttons=buttons)
+
+        return []
+
+    def get_button_title(
+        self, intent: Text, entities: Dict[Text, Text]
+    ) -> Text:
+        default_utterance_query = self.intent_mappings.intent == intent
+        utterance_query = (
+            self.intent_mappings.entities == entities.keys()
+        ) & (default_utterance_query)
+
+        utterances = self.intent_mappings[utterance_query].button.tolist()
+
+        if len(utterances) > 0:
+            button_title = utterances[0]
+        else:
+            utterances = self.intent_mappings[
+                default_utterance_query
+            ].button.tolist()
+            button_title = utterances[0] if len(utterances) > 0 else intent
+
+        return button_title.format(**entities)
+
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+
+        # Fallback caused by TwoStageFallbackPolicy
+        if (
+            len(tracker.events) >= 4
+            and tracker.events[-4].get("name")
+            == "action_default_ask_affirmation"
+        ):
+
+            dispatcher.utter_message(template="utter_restart_with_button")
+
+            return [
+                SlotSet("feedback_value", "negative"),
+                ConversationPaused(),
+            ]
+
+        # Fallback caused by Core
+        else:
+            dispatcher.utter_message(template="utter_default")
+            return [UserUtteranceReverted()]
