f816fc374e14811c21b7f453e7f529814bfe53c1
minafsh <m.foosherian@yahoo.com>
2020-10-25 23:50:46 +0100

    label fatigue confusion improved

--- actions/repair.py
+++ actions/repair.py
@@ -160,8 +160,8 @@ class ActionRepairLabelFatigueConfusion(Action):
                 conv_turns +=1
         
         #logger.debug(f"This is the tracker after restart: {tracker.events_after_latest_restart()}")
-        logger.debug(f"There has been these events from user so far: {user_event_list}")
-        logger.debug(f"There has been {conv_turns} turns in this conversation so far!")
+        #logger.debug(f"There has been these events from user so far: {user_event_list}")
+        #logger.debug(f"There has been {conv_turns} turns in this conversation so far!")
 
         # Calculate confusion based on jumping from one intent to another
         distanced_intent_list = [["greet", "bye", "nicetomeetyou", "restart"], 
@@ -184,23 +184,66 @@ class ActionRepairLabelFatigueConfusion(Action):
         for list in distanced_intent_list:
             if last_intent_name in list:
                 index_1 = distanced_intent_list.index(list)
-                logger.debug(f"{index_1}")
+                logger.debug(f"{last_intent_name} is in group {index_1}.")
 
             if second_last_intent_name in list:
                 index_2 = distanced_intent_list.index(list)
-                logger.debug(f"{index_2}")
+                logger.debug(f"{second_last_intent_name} is in group {index_2}.")
 
         confusion_level = 0
         if index_1 != index_2:
             confusion_level +=1
-            logger.debug(f"{confusion_level}")
+            logger.debug(f"The confusion level is: {confusion_level}")
 
-        if conv_turns <= 6:
-            return [FollowupAction("action_repair_options")]
-        elif conv_turns > 6:
-            dispatcher.utter_message(template="utter_fatigue")
-            return [FollowupAction("action_listen")]
-        return[]
+        # if conv_turns <= 6:
+        #     return [FollowupAction("action_repair_options")]
+        # elif conv_turns > 6:
+        #     dispatcher.utter_message(template="utter_fatigue")
+        #     return [FollowupAction("action_listen")]
+        # return[]
+
+
+        intent_ranking = tracker.latest_message.get("intent_ranking", [])
+        if len(intent_ranking) > 1:
+            highest_ranked_intent_confidence = intent_ranking[0].get("confidence")
+            highest_ranked_intent_name = intent_ranking[0].get("name")
+            if highest_ranked_intent_confidence >= 0.75:
+                if conv_turns <= 10:
+                    # bot is not tired, and is somehow sure about the intent.
+                    if confusion_level < 1:
+                        # bot is not foncused
+                        message_title = f"I'm Highly confident that you mean {last_intent_name}."
+                    else:
+                        # bot is confused
+                        message_title = f"I'm Highly confident that you mean {last_intent_name}. but you switched the topic from {second_last_intent_name}. this can confuse me in future. "
+                else:
+                    # bot is tired, and is somehow sure about the intent.
+                    if confusion_level < 1:
+                        # bot is not confused
+                        message_title = f"I'm Highly confident that you mean {last_intent_name}. but our conversation is getting long and I'm tired."
+                    else:
+                        # bot is confused
+                        message_title = f"I'm Highly confident that you mean {last_intent_name}. but you switched the topic from {second_last_intent_name}. this can confuse me in future. Our conversation is also getting long and I'm tired. "
+            else:
+                if conv_turns <= 10:
+                    # bot is not tired, and is not sure about the intent.
+                    if confusion_level < 1:
+                        # bot is not foncused
+                        message_title = f"I'm not so sure if you mean {last_intent_name}."
+                    else:
+                        # bot is confused
+                        message_title = f"I'm not so sure if you mean {last_intent_name}. but you switched the topic from {second_last_intent_name}. this confused me ."
+                else:
+                    # bot is tired, and is not sure about the intent.
+                    if confusion_level < 1:
+                        # bot is not confused
+                        message_title = f"I'm not so sure if you mean {last_intent_name}. Our conversation is getting long and I'm tired. maybe that's why I can't get you."
+                    else:
+                        # bot is confused
+                        message_title = f"I'm not so sure if you mean {last_intent_name}. but you switched the topic from {second_last_intent_name}. this confused me. Our conversation is also getting long and I'm tired. could be also why I can't get you."
+        
+        dispatcher.utter_message(text=message_title)
+        return [FollowupAction("action_listen")]
 
 
 class ActionRepairLabelUserUtteranceLenght(Action):
