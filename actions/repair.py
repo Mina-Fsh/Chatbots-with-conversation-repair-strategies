4319583739cdea995b49514253820314c281d6f3
minafsh <m.foosherian@yahoo.com>
2021-01-05 00:17:13 +0100

    restart with button works now

--- actions/repair.py
+++ actions/repair.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List, Text
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import (
-    FollowupAction
+    FollowupAction,
 )
 
 INTENT_DESCRIPTION_MAPPING_PATH = "actions/intent_description_mapping.csv"
@@ -65,14 +65,16 @@ class ActionRepair(Action):
         # element -15 to the end of the list ":"
         # https://stackoverflow.com/questions/9542738/python-find-in-list
 
-        logger.info(f'clause: {next((True for event in tracker.events[-15:] if event.get("name") == "system_repair"), False) }')
+        events = tracker.events[-15:]
+        logger.info(f"Events are: {events}")
+        logger.info(f'clause: {next((True for event in tracker.events[-9:] if event.get("name") == "action_system_repair"), False) }')
 
         repair_strategy = tracker.get_slot("repair_strategy_name")
 
         # Fallback caused by TwoStageFallbackPolicy
         if (
-            len(tracker.events) >= 15 and
-            next((True for event in tracker.events[-15:] if event.get("name") == "action_repair" and repair_strategy == "system_repair"), False)
+            len(tracker.events) >= 4
+            and next((True for event in tracker.events[-9:] if event.get("name") == "action_system_repair"), False)
         ):
 
             dispatcher.utter_message(template="utter_restart_with_button")
