11b20235d3a793183aa7ba5b8162a62c249b1937
minafsh <m.foosherian@yahoo.com>
2021-02-01 15:30:49 +0100

    two stage fallback policy improved

--- actions/repair_nlu.py
+++ actions/repair_nlu.py
@@ -4,8 +4,6 @@ from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import (
     FollowupAction,
-    EventType,
-    UserUtteranceReverted,
 )
 
 INTENT_DESCRIPTION_MAPPING_PATH = "actions/intent_description_mapping.csv"
@@ -25,50 +23,40 @@ class ActionRepair(Action):
         domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
 
-        logger.info(f"length: { len(tracker.events) }")
-
-        # Checks a list consisting of the last four elements => starting from
-        # element -15 to the end of the list ":"
-        # https://stackoverflow.com/questions/9542738/python-find-in-list
-
-        restart_repairs = ["action_system_repair", "action_mix_repair"]
-        events = tracker.events[-15:]
-        logger.info(f"Events are: {events}")
-        logger.info(f'clause: {next((True for event in tracker.events[-9:] if event.get("name") in restart_repairs), False) }')
+        action_event_list = []
+        for events in tracker.events_after_latest_restart():
+            if events["event"] == "action":
+                action_event_list.append(events)
+        action_event_list_clean = [i for i in action_event_list if not (i['name'] == 'action_listen')]
+        logger.info(f"Cleaned action events are: {action_event_list_clean}")
+        # {'event': 'action', 'timestamp': 1612187448.9603305, 'name': 'utter_rephrase', 'policy': 'policy_2_RulePolicy', 'confidence': 1.0, 'action_text': None}, 
 
         repair_strategy = tracker.get_slot("repair_strategy_name")
 
-        # Fallback caused by TwoStageFallbackPolicy
-        if (
-            len(tracker.events) >= 12
-            and next((True for event in tracker.events[-9:] if event.get("name") in restart_repairs), False)
-        ):
-
-            dispatcher.utter_message(template="utter_restart_with_button")
-
-            return []
-        #######
-
         if repair_strategy == "system_repair":
-            return [FollowupAction("action_system_repair")]
+            # Fallback caused by TwoStageFallbackPolicy
+            last_action_needed_for_two_stage = ["action_system_repair", "utter_rephrase"]
+            if (
+                len(action_event_list) >= 2
+                and action_event_list_clean[-1].get("name") in last_action_needed_for_two_stage
+            ):
+                dispatcher.utter_message(template="utter_restart_with_button")
+                return []
+            else:
+                return [FollowupAction("action_system_repair")]
+        elif repair_strategy == "mix_repair":
+            # Fallback caused by TwoStageFallbackPolicy
+            last_action_needed_for_two_stage = ["action_mix_repair", "utter_rephrase"]
+            if (
+                len(action_event_list) >= 2
+                and action_event_list_clean[-1].get("name") in last_action_needed_for_two_stage
+            ):
+                dispatcher.utter_message(template="utter_restart_with_button")
+                return []
+            else:
+                return [FollowupAction("action_mix_repair")]
         elif repair_strategy == "self_assisted_repair":
             return [FollowupAction("action_self_assisted_repair")]
-        elif repair_strategy == "mix_repair":
-            return [FollowupAction("action_mix_repair")]
         else:
             return [FollowupAction("action_system_repair")]
         return []
-
-
-class ActionRephrase(Action):
-    """reverts the last user message to keep
-    tracker clean when user says they mean something else!"""
-
-    def name(self) -> Text:
-        return "action_rephrase"
-
-    def run(self, dispatcher, tracker, domain) -> List[EventType]:
-
-        dispatcher.utter_message(template="utter_rephrase")
-
-        return [UserUtteranceReverted()]
